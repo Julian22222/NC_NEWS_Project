09f99750ddc5e6c6906121f9812092f00f043103
Julian22222 <julik.golovenj@gmail.com>
2025-05-30 15:43:20 +0100

    notes

--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 
+//API PORT
 const { PORT = 9030 } = process.env; // If PORT is not set, the default value = 9030 / destructuring the PORT value from process.env.
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
